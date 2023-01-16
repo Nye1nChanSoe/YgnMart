e550dfd5c1c861bc8cda62b051601ffdf9de2f38
NyeinChan Soe <nyeinchansoe.business@gmail.com>
2023-01-16 21:22:55 +0630

    Add models, migration files and basic html

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Home Page</title>
+</head>
+<body>
+    <section>
+        <header>
+            <div>
+                {{-- temporary logo might change later --}}
+                <a href="/"><img src="/images/logos/main_logo.png" alt=""></a>
+            </div>
+            <div>
+                <form action="" method="GET">
+                    <input type="text" placeholder="Search products and categories..">
+                </form>
+            </div>
+            <div>
+                {{-- Languages --}}
+                <div>
+                    <select name="lang_preference" id="">
+                        <option value="en">EN</option>
+                        <option value="mm">MM</option>
+                    </select>
+                </div>
+                <a href=""><i class="fas fa-shopping-cart"></i></a>
+                <a href="">Login</a>
+            </div>
+        </header>
+        
+        <nav>
+            <div>
+                <select name="" id="">
+                    <option value="">Foods</option>
+                    <option value="">Foods</option>
+                    <option value="">Foods</option>
+                    <option value="">Foods</option>
+                    <option value="">Foods</option>
+                </select>
+            </div>
+            <div>
+                <select name="" id="">
+                    <option value="">Drinks</option>
+                    <option value="">Drinks</option>
+                    <option value="">Drinks</option>
+                    <option value="">Drinks</option>
+                    <option value="">Drinks</option>
+                </select>
+            </div>
+            <div>
+                <select name="" id="">
+                    <option value="">Household products</option>
+                    <option value="">Household products</option>
+                    <option value="">Household products</option>
+                    <option value="">Household products</option>
+                    <option value="">Household products</option>
+                </select>
+            </div>
+            <div>
+                <select name="" id="">
+                    <option value="">Discounts</option>
+                    <option value="">Discounts</option>
+                    <option value="">Discounts</option>
+                    <option value="">Discounts</option>
+                    <option value="">Discounts</option>
+                </select>
+            </div>
+            <div>
+                <select name="" id="">
+                    <option value="">Popular products</option>
+                    <option value="">Popular products</option>
+                    <option value="">Popular products</option>
+                    <option value="">Popular products</option>
+                    <option value="">Popular products</option>
+                </select>
+            </div>
+            <div>
+                <select name="" id="">
+                    <option value="">Info</option>
+                    <option value="">Info</option>
+                    <option value="">Info</option>
+                    <option value="">Info</option>
+                    <option value="">Info</option>
+                </select>
+            </div>
+            <div>
+                <select name="" id="">
+                    <option value="">Our Shops</option>
+                    <option value="">Our Shops</option>
+                    <option value="">Our Shops</option>
+                    <option value="">Our Shops</option>
+                    <option value="">Our Shops</option>
+                </select>
+            </div>
+        </nav>
+
+        <main>
+            {{-- hero / carousel --}}
+            <section>
+                <img src="" alt="">
+            </section>
+
+            {{-- products --}}
+            <section>
+                <div>
+                    Main featured product
+                </div>
+                <div>
+                    <div>
+                        Second featured product
+                    </div>
+                    <div>
+                        Second featured product
+                    </div>
+                </div>
+                {{-- 3 --}}
+                <div>
+                    <div>
+                        Product1
+                    </div>
+                    <div>
+                        Product2
+                    </div>
+                    <div>
+                        Product3
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        {{-- footer will have 4 columns --}}
+        <footer>
+            <section>
+                <div>
+                    <div>
+                        <div>
+                            <a href=""><img src="/images/logos/main_logo.png" alt=""></a>
+                        </div>
+                        <div>
+                            <h3>Mission</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facilis excepturi recusandae eveniet atque debitis, labore quas in praesentium iusto, tempore officia ut! Dolorem placeat sed alias aliquam ab aut?
+                            </p>
+                        </div>
+                        <div>
+                            <h3>History</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat qui eos ad sunt possimus nulla. Dolores unde non, dolore obcaecati saepe architecto aperiam, quam exercitationem magni repellendus maiores veritatis. Inventore!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>Services</h3>
+                        <ul>    
+                            <li><a href="">Vendor</a></li>
+                            <li><a href="">Rules</a></li>
+                            <li><a href="">Guidelines</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>Customer Service</h3>
+                        <ul>    
+                            <li><a href="">Vendor</a></li>
+                            <li><a href="">Rules</a></li>
+                            <li><a href="">Guidelines</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>Enquries</h3>
+                        <ul>
+                            <li><a href="">Phone No: 0969696969</a></li>
+                            <li><a href="">Email: callme@hotmail.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <div>Copyright &copy; 2023 Yangon Mart</div>
+        </footer>
+    </section>
+</body>
+</html>
