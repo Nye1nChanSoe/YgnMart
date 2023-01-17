a583a93f80f7e4cc3511233145e5e8fc62a22bfe
NyeinChan Soe <nyeinchansoe.business@gmail.com>
2023-01-17 21:51:45 +0630

    Add tailwind classes to products.index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,182 +11,130 @@
 </head>
 <body>
     <section>
-        <header>
-            <div>
-                {{-- temporary logo might change later --}}
-                <a href="/"><img src="/images/logos/main_logo.png" alt=""></a>
-            </div>
-            <div>
-                <form action="" method="GET">
-                    <input type="text" placeholder="Search products and categories..">
-                </form>
-            </div>
-            <div>
-                {{-- Languages --}}
-                <div>
-                    <select name="lang_preference" id="">
-                        <option value="en">EN</option>
-                        <option value="mm">MM</option>
-                    </select>
+        <header class="space-y-3 mb-10">
+            <div class="flex justify-between items-center px-8 py-4">
+                <figure>
+                    {{-- temporary logo might change later --}}
+                    <a href="/"><img src="/images/logos/main_logo.svg" alt="" width="100" height=""></a>
+                </figure>
+                {{-- search --}}
+                <div class="flex-grow px-36 3xl:px-96">
+                    <div class="relative border border-gray-400 rounded-xl">
+                        <div class="absolute top-2.5 left-3">
+                            <button type="submit">
+                                <i class="fa fa-search text-gray-500 z-20 hover:text-gray-700"></i>
+                            </button>
+                        </div>
+                        <input type="text" name="search" class="w-full pl-10 pr-20 py-2 rounded-xl focus:ring-1 focus:ring-gray-800 focus:outline-none" placeholder="Search everything you need">
+                    </div>
                 </div>
-                <a href=""><i class="fas fa-shopping-cart"></i></a>
-                <a href="">Login</a>
+                <div class="flex items-center space-x-8">
+                    <div>
+                        <select name="lang_preference" id="">
+                            <option value="en">EN</option>
+                            <option value="mm">MM</option>
+                        </select>
+                    </div>
+                    <a href="" class="hover:text-blue-600" id="cart"><i class="fas fa-shopping-cart"></i></a>
+                    <a href="" class="text-lg hover:text-blue-600">Login</a>
+                </div>
             </div>
+
+            <nav class="container mx-auto flex items-center justify-between mb-10 px-4">
+                <div class="relative bg-slate-100 rounded-xl">
+                    {{-- hard coded width w-32 --}}
+                    <button class="bg-transparent py-2 px-3 text-left text-sm font-semibold w-full lg:w-32">
+                        Category
+                        {{-- svgs are block by default in tailwind --}}
+                        <svg class="inline transform -rotate-90 absolute pointer-events-none right-2" width="22" height="22" viewBox="0 0 22 22">
+                            <g fill="none" fill-rule="evenodd">
+                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z"></path>
+                                <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                            </g>
+                        </svg>
+                    </button> 
+                    {{--TODO: toggle visibility with js --}}
+                    {{-- position absolute so it won't distrub the page flow --}}
+                    <div class="hidden absolute py-2 bg-gray-50 w-full mt-1 max-h-52 overflow-auto rounded-xl z-50">
+                        <a href="" class="block text-left px-3 text-sm leading-8 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focuse:text-white">Foods</a>
+                        <a href="" class="block text-left px-3 text-sm leading-8 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focuse:text-white">Foods</a>
+                        <a href="" class="block text-left px-3 text-sm leading-8 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focuse:text-white">Foods</a>
+                        <a href="" class="block text-left px-3 text-sm leading-8 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focuse:text-white">Foods</a>
+                        <a href="" class="block text-left px-3 text-sm leading-8 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focuse:text-white">Foods</a>
+                        <a href="" class="block text-left px-3 text-sm leading-8 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focuse:text-white">Foods</a>
+                    </div>
+                </div>
+            </nav>
         </header>
         
-        <nav>
-            <div>
-                <select name="" id="">
-                    <option value="">Foods</option>
-                    <option value="">Foods</option>
-                    <option value="">Foods</option>
-                    <option value="">Foods</option>
-                    <option value="">Foods</option>
-                </select>
-            </div>
-            <div>
-                <select name="" id="">
-                    <option value="">Drinks</option>
-                    <option value="">Drinks</option>
-                    <option value="">Drinks</option>
-                    <option value="">Drinks</option>
-                    <option value="">Drinks</option>
-                </select>
-            </div>
-            <div>
-                <select name="" id="">
-                    <option value="">Household products</option>
-                    <option value="">Household products</option>
-                    <option value="">Household products</option>
-                    <option value="">Household products</option>
-                    <option value="">Household products</option>
-                </select>
-            </div>
-            <div>
-                <select name="" id="">
-                    <option value="">Discounts</option>
-                    <option value="">Discounts</option>
-                    <option value="">Discounts</option>
-                    <option value="">Discounts</option>
-                    <option value="">Discounts</option>
-                </select>
-            </div>
-            <div>
-                <select name="" id="">
-                    <option value="">Popular products</option>
-                    <option value="">Popular products</option>
-                    <option value="">Popular products</option>
-                    <option value="">Popular products</option>
-                    <option value="">Popular products</option>
-                </select>
-            </div>
-            <div>
-                <select name="" id="">
-                    <option value="">Info</option>
-                    <option value="">Info</option>
-                    <option value="">Info</option>
-                    <option value="">Info</option>
-                    <option value="">Info</option>
-                </select>
-            </div>
-            <div>
-                <select name="" id="">
-                    <option value="">Our Shops</option>
-                    <option value="">Our Shops</option>
-                    <option value="">Our Shops</option>
-                    <option value="">Our Shops</option>
-                    <option value="">Our Shops</option>
-                </select>
-            </div>
-        </nav>
-
+        {{-- TODO: set timer with js --}}
+        {{-- hero / carousel --}}
+        <section>
+            <figure class="h-96 bg-slate-100 flex items-center justify-center mb-20">
+                <img src="/images/logos/main_logo.svg" alt="" width="500">
+            </figure>
+        </section>
+        
         <main>
-            {{-- hero / carousel --}}
-            <section>
-                <img src="" alt="">
-            </section>
+            <div>
 
-            {{-- products --}}
-            <section>
-                <div>
-                    Main featured product
-                </div>
-                <div>
-                    <div>
-                        Second featured product
-                    </div>
-                    <div>
-                        Second featured product
-                    </div>
-                </div>
-                {{-- 3 --}}
-                <div>
-                    <div>
-                        Product1
-                    </div>
-                    <div>
-                        Product2
-                    </div>
-                    <div>
-                        Product3
-                    </div>
-                </div>
-            </section>
+            </div>
         </main>
 
-        {{-- footer will have 4 columns --}}
-        <footer>
-            <section>
-                <div>
-                    <div>
+        
+
+        <footer class="bg-slate-100">
+            <div class="container mx-auto flex flex-col text-slate-600 p-10">
+                <section class="flex justify-between items-center mb-4">
+                    <div class="space-y-5">
+                            <p class="text-sm">We accept multiple payments</p>
+                            <h2 class="text-xl font-bold">Start shopping with Yangon Mart today.</h2>
+                        <a href="" class="block w-full bg-blue-600 p-2 rounded-xl text-center shadow-lg text-white hover:bg-blue-700">Sign up</a>
+                    </div>
+                    
+                    <figure class="mx-auto">
+                        <img src="/images/shopping_cart.png" alt="" width="240">
+                    </figure>
+                </section>
+
+                <section class="flex justify-between items-end">
+                    <div class="space-y-3">
+                        <figure>
+                            <a href="/"><img src="/images/logos/main_logo.svg" alt="" width="100"></a>
+                        </figure>
+                        <p class="italic tracking-wider font-semibold">Shop anytime, anywhere with us</p>
                         <div>
-                            <a href=""><img src="/images/logos/main_logo.png" alt=""></a>
+                            <div>
+                                <i class="fa-solid fa-comment-dots"></i>
+                                <a href="tel:+959771637812" class="hover:text-black">+95 9 771 637 812</a>
+                            </div>
+                            <div>
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                <a href="mailto:enquiry@ygnmart.com" class="hover:text-black">enquiry@ygnmart.com</a>
+                            </div>
                         </div>
-                        <div>
-                            <h3>Mission</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facilis excepturi recusandae eveniet atque debitis, labore quas in praesentium iusto, tempore officia ut! Dolorem placeat sed alias aliquam ab aut?
-                            </p>
+                        <p class="text-sm">Copyright &copy; 2023 Yangon Mart</p>
+                    </div>
+                    <div class="flex flex-col">
+                        <h3 class="font-semibold mb-2">About</h3>
+                        <a href="" class="hover:text-black">Customer Service</a>
+                        <a href="" class="hover:text-black">Vendor Guidelines</a>
+                    </div>
+                    <div class="flex flex-col">
+                        <h3 class="font-semibold mb-2">Terms and Conditions</h3>
+                        <a href="" class="hover:text-black">Privacy Policy</a>
+                        <a href="" class="hover:text-black">Cookie Policy</a>
+                    </div>
+                    <div>
+                        <h3 class="font-semibold mb-2">Platforms</h3>
+                        <div class="space-x-3 text-xl">
+                            <a href="#" class="hover:text-black"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a href="#" class="hover:text-black"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#" class="hover:text-black"><i class="fa-brands fa-viber"></i></a>
                         </div>
-                        <div>
-                            <h3>History</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat qui eos ad sunt possimus nulla. Dolores unde non, dolore obcaecati saepe architecto aperiam, quam exercitationem magni repellendus maiores veritatis. Inventore!
-                            </p>
-                        </div>
                     </div>
-                </div>
-                <div>
-                    <div>
-                        <h3>Services</h3>
-                        <ul>    
-                            <li><a href="">Vendor</a></li>
-                            <li><a href="">Rules</a></li>
-                            <li><a href="">Guidelines</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <h3>Customer Service</h3>
-                        <ul>    
-                            <li><a href="">Vendor</a></li>
-                            <li><a href="">Rules</a></li>
-                            <li><a href="">Guidelines</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <h3>Enquries</h3>
-                        <ul>
-                            <li><a href="">Phone No: 0969696969</a></li>
-                            <li><a href="">Email: callme@hotmail.com</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-            <div>Copyright &copy; 2023 Yangon Mart</div>
+                </section>
+            </div>
         </footer>
     </section>
 </body>
